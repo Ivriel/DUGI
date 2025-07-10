22dd30b0259f712532997a473478bfa888ee5ec5
ledyjn <ledyjentrimeiza@gmail.com>
2025-07-10 17:59:21 +0700

    cimmit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,41 +1,41 @@
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
 import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  loginObj:any = {
-    email:"",
-    password:""
+  loginObj: any = {
+    email: '',
+    password: '',
+  };
+
+  constructor(private authService: AuthService, private title: Title) {
+    this.title.setTitle('Login');
   }
 
-  constructor(private authService:AuthService,private title:Title){
-    this.title.setTitle("Login")
-  }
-
-  onLoginSubmit(form:NgForm){
-    console.log("Form Value: ",form.value)
-    console.log("Login Obj: ",this.loginObj)
+  onLoginSubmit(form: NgForm) {
+    console.log('Form Value: ', form.value);
+    console.log('Login Obj: ', this.loginObj);
     console.log('Data yang akan dikirim ke API:', this.loginObj);
     this.authService.sendDataLogin(this.loginObj).subscribe({
-      next:(res:any)=> {
-        alert("Login berhasil")
-        console.log("Login berhasil: ",res)
-        if(res.accessToken) {
-          this.authService.setToken(res.accessToken)
+      next: (res: any) => {
+        alert('Login berhasil');
+        console.log('Login berhasil: ', res);
+        if (res.accessToken) {
+          this.authService.setToken(res.accessToken);
         }
       },
-      error:(error)=> {
-        console.error("Login gagal: ",error)
-        alert("Login gagal")
-      }
-    })
-
+      error: (error) => {
+        console.error('Login gagal: ', error);
+        alert('Login gagal');
+      },
+    });
   }
 }
