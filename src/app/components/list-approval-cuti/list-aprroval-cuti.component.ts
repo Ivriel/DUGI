5def510f8445d9eaa336ba05869190316ad1d8d6
kyaraa <najwakyara0@gmail.com>
2025-07-11 16:43:07 +0700

    deleting directive import

--- src/app/components/list-approval-cuti/list-aprroval-cuti.component.ts
+++ src/app/components/list-approval-cuti/list-aprroval-cuti.component.ts
@@ -4,12 +4,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { CutiService } from '../../services/list-approval-cuti.service';
 import { AuthService } from '../../services/auth.service';
 
-import { CommonModule, DatePipe, NgClass, NgFor, NgIf } from '@angular/common';
+import { CommonModule, DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-list-approval-cuti',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, NgClass, NgFor, NgIf],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './list-approval-cuti.component.html',
   styleUrls: ['./list-approval-cuti.component.css'],
   providers: [DatePipe],
@@ -32,7 +32,7 @@ export class ListApprovalCutiComponent implements OnInit {
       companyId: [0],
       employeeId: [0],
       status: ['All'],
-      pageSize: [10],
+      pageSize: [20],
       pageNumber: [1],
     });
   }
